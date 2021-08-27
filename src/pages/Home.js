f4f17233fdbc36402472dfd0622a0ec2e3876174
Vien-world <vien@world-works.co.jp>
2021-08-27 11:53:00 +0900

    move folder

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,16 @@
 import React from "react";
+import TireProfile from "../components/TireProfile";
+import tireData from "../data/tireInfo.json";
 
+const data = tireData;
 function Home(props) {
-  return <div>Home</div>;
+  return (
+    <div>
+      {data.tireProfile.map((items, index) => {
+        return <h2>{items.name}</h2>;
+      })}
+    </div>
+  );
 }
 
 export default Home;
