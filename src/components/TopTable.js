b96dccee9df4b55b17b80ac7d62f4ca112dad751
Vien-world <vien@world-works.co.jp>
2021-08-30 13:02:39 +0900

    fix css for TireTable

--- src/components/TopTable.js
+++ src/components/TopTable.js
@@ -5,7 +5,7 @@ import data from "../data/topTable.json";
 const useStyles = makeStyles({
   table: {
     width: "95%",
-    margin: "auto",
+    margin: "10px auto 0px auto",
     borderCollapse: "collapse",
     "& th, td": {
       border: "1px solid black",
