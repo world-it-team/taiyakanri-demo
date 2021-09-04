2651aa74d86997359f620ba89764dde46c3edb3e
theanhjp <anh@world-works.co.jp>
2021-09-04 13:47:50 +0900

    app.js update

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import "./App.css";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import API, { graphqlOperation } from "@aws-amplify/api";
+import { listTireCheckinfos } from "./graphql/queries";
 
-
+async function test() {
+  API.graphql(graphqlOperation(listTireCheckinfos)).then((data) => {
+    console.log(data)
+  });
+}
 
 function App() {
+  test();
   return (
     <Router>
       <Route path="/">
